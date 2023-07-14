ce9b8b414a287363ea5380723d3da60e9c295ad9
yanlingca <cling23@my.centennialcollege.ca>
2023-07-14 11:44:46 -0400

    create log model

---
+++ models/log.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const LogSchema = mongoose.Schema({
+    LogNo : Integer,
+    CreatedDate : {
+        type: Date,
+        default: Date.now
+    },
+    ItemDesc : String,
+    TurnedInBy : String,
+    ClaimedBy : String,
+    Phone : String,
+    ReleasedBy : String,
+    DateReleased : Date
+});
+
+module.exports = mongoose.model('Log', LogSchema);
