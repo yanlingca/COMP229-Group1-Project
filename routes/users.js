a57b3bf099bb1790043f91bdd5a9bb2cb58d8d36
RainyCa <129433618+RainyCa@users.noreply.github.com>
2023-07-19 18:14:47 -0400

    add logout/login and register button on navbar and ensure functionality

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var User = require('../models/user');
+var User = require("../models/user");
 const mongoose = require("mongoose");
-const bcrypt= require('bcrypt');
-const {login, checkLoggedIn} =require("../middleware/auth");
+const bcrypt = require("bcrypt");
+const { login, checkLoggedIn } = require("../middleware/auth");
 
 /*GET registration page*/
 router.get("/registration", function (req, res, next) {
-  res.cookie("jwt", "", { maxAge: "1" })
-  res.render("index", { title: "Registration", reg_message: "", loggedIn: false});
+  res.cookie("jwt", "", { maxAge: "1" });
+  res.render("index", {
+    title: "Registration",
+    reg_message: "",
+    loggedIn: false,
+  });
 });
 
 /*POST create new user*/
-router.post("/registration", async (req, res, next) =>{
-  const hashedPassword = await bcrypt.hash(req.body.password,10);
+router.post("/registration", async (req, res, next) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
   const user = new User({
-    first_name : req.body.first_name,
-    last_name : req.body.last_name,
-    email : req.body.email,
-    password : hashedPassword
-  })
-    try {
-      const newUser = await user.save();
-      res.redirect('../');
-    } catch (err) {
-      res.render("index", { title: "Registration", reg_message: "Failed to register. Please try again!"});
-      console.log(err.message);
-    }
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const newUser = await user.save();
+    res.redirect("../");
+  } catch (err) {
+    res.render("index", {
+      title: "Registration",
+      reg_message: "Failed to register. Please try again!",
+    });
+    console.log(err.message);
+  }
 });
 
 /*GET login page*/
 router.get("/login", checkLoggedIn, function (req, res, next) {
-  res.cookie("jwt", "", { maxAge: "1" })
+  res.cookie("jwt", "", { maxAge: "1" });
   res.render("index", { title: "Login", login_message: "", loggedIn: false });
 });
 
 //Handling user login
 router.route("/login").post(login);
 
-//Handling user logout 
+//Handling user logout
 router.get("/logout", (req, res) => {
-  res.cookie("jwt", "", { maxAge: "1" })
-  res.redirect("/")
-})
+  res.cookie("jwt", "", { maxAge: "1" });
+  res.redirect("/");
+});
 
 module.exports = router;
